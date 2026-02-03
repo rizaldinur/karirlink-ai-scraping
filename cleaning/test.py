bfcfdec1dbd93f1cc0d5f322059fcb40a730c970
rizaldinur <rnur71@gmail.com>
2026-02-03 16:20:28 +0700

    fix: output to stdout/err as json

--- cleaning/test.py
+++ cleaning/test.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import os
 import sys
@@ -10,13 +11,15 @@ NAMA_FILE_INPUT = sys.argv[1]
 
 # --- 1. SETUP LOKASI FILE ---
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, '..', 'storage', NAMA_FILE_INPUT)
+file_path = os.path.join(script_dir, '..', 'storage',NAMA_FILE_INPUT)
 
-print(f" Memproses file: {file_path}")
 
 if not os.path.exists(file_path):
-    print(" ERROR: File tidak ditemukan.")
-    exit()
+    print(json.dumps({
+        "status": "error",
+        "message": "File tidak ditemukan"
+    }),file=sys.stderr)
+    sys.exit(1)
 
 # --- 2. BACA CSV (DELIMITER ,) ---
 df = pd.read_csv(
@@ -26,15 +29,15 @@ df = pd.read_csv(
     on_bad_lines='skip'
 )
 
-print(f" Total data awal: {len(df)}")
-print(" Kolom terbaca:", df.columns.tolist())
+# print(f" Total data awal: {len(df)}")
+# print(" Kolom terbaca:", df.columns.tolist())
 
 # --- 3. HAPUS HANYA success = false ---
 if 'success' in df.columns:
     df['success'] = df['success'].astype(str).str.lower().str.strip()
     before = len(df)
     df = df[df['success'] != 'false']
-    print(f" Data success=false dibuang: {before - len(df)}")
+    # print(f" Data success=false dibuang: {before - len(df)}")
 
 # --- 4. HAPUS DUPLIKAT (title + location + jobDescription) ---
 kolom_duplikat = ['title', 'location', 'jobDescription']
@@ -42,13 +45,19 @@ kolom_duplikat = ['title', 'location', 'jobDescription']
 if all(col in df.columns for col in kolom_duplikat):
     before = len(df)
     df = df.drop_duplicates(subset=kolom_duplikat, keep='first')
-    print(f" Data duplikat dibuang: {before - len(df)}")
-else:
-    print(" Kolom duplikat tidak lengkap, skip step ini")
+    # print(f" Data duplikat dibuang: {before - len(df)}")
+# else:
+#     print(" Kolom duplikat tidak lengkap, skip step ini")
 
 # --- 5. SIMPAN HASIL (NAMA BARU BIAR AMAN) ---
 output_name = NAMA_FILE_INPUT.replace('.csv', '_CLEANED_FINAL_V2.csv').replace('test-result-', 'RESULT-')
 output_file = os.path.join(script_dir, '..', 'storage', output_name)
 
 df.to_csv(output_file, index=False)
-print(f" SELESAI! File tersimpan di:\n{output_file}")
+# print(f" SELESAI! File tersimpan di:\n{output_file}")
+print(json.dumps({
+    "status": "ok",
+    "message": "Berhasil cleaning file",
+    "output_file": output_name,
+    "output_path": output_file
+}))
